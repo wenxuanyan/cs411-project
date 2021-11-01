c294aa5c2946514654dba39ea950ce3932773d34
Li-Haowei <91715036+Li-Haowei@users.noreply.github.com>
2021-11-01 18:22:31 -0400

    Update customer.java
    added comments of each methods;
    added more tests for the main method;

--- customer.java
+++ customer.java
@@ -1,51 +1,100 @@
-package resources;
+import java.util.Arrays;
 
 public class customer {
 	private String name;
 	private String id;
 	private String phoneNumber;
 	private String[] paymentInfo;
+	/**
+	 * This method gets the name of the customer
+	 * @return name of the customer
+	 */
 	public final String getName(){
 		return name;
 	}
+	/**
+	 * set the name of the current customer object
+	 * @param name
+	 * @throws IllegalArgumentException
+	 */
 	public final void setName(String name) throws IllegalArgumentException{
 		if(name==null || name.length() == 0)
 			throw new IllegalArgumentException("Name can not be NULL or length equals 0");
 		this.name = name;
 	}
+	/**
+	 * This method returns the unique ID of the customer
+	 * @return ID
+	 */
 	public final String getId(){
 		return id;
 	}
+	/**
+	 * This method sets the id for the customer
+	 * @param id
+	 * @throws IllegalArgumentException
+	 */
 	public final void setId(String id) throws IllegalArgumentException{
 		if(id==null || id.length() == 0)
 			throw new IllegalArgumentException("id can not be NULL or length equals 0");
 		this.id = id;
 	}
+	/**
+	 * This method returns a phone number of the customer
+	 * @return phoneNumber
+	 */
 	public final String getPhoneNumber(){
 		return phoneNumber;
 	}
+	/**
+	 * This method sets a phone number for the customer object
+	 * @param phoneNumber
+	 * @throws IllegalArgumentException
+	 */
 	public final void setPhoneNumber(String phoneNumber) throws IllegalArgumentException{
 		if(phoneNumber==null || phoneNumber.length() == 0)
 			throw new IllegalArgumentException("phoneNumber can not be NULL or length equals 0");
 		this.phoneNumber = phoneNumber;
 	}
+	/**
+	 * This method returns the customer payment information
+	 * @return paymentInfo
+	 */
 	public final String[] getPaymentInfo(){
 		return paymentInfo;
 	}
+	/**
+	 * This methods sets a payment information for the customer
+	 * @param paymentInfo
+	 * @throws IllegalArgumentException
+	 */
 	public final void setPaymentInfo(String[] paymentInfo) throws IllegalArgumentException{
 		if(paymentInfo==null || paymentInfo.length == 0)
 			throw new IllegalArgumentException("phoneNumber can not be NULL or length equals 0");
 		this.paymentInfo = paymentInfo;
 	}
+	/**
+	 * This creates an empty customer object, and it needs to be added information
+	 */
 	public customer() {
 		
 	}
+	/**
+	 * This is the official constructor for a customer object
+	 * @param name
+	 * @param id
+	 * @param phoneNumber
+	 * @param paymentInfo
+	 */
 	public customer(String name, String id, String phoneNumber, String[] paymentInfo) {
 		setName(name);
 		setId(id);
 		setPhoneNumber(phoneNumber);
 		setPaymentInfo(paymentInfo);
 	}
+	/**
+	 * This main function tests the class to see if it works
+	 */
 	public void main() {
 		
 		paymentInfo = new String[]{"BoA","12345","checking"};
@@ -53,6 +102,6 @@ public class customer {
 		System.out.println(customer1.getName());
 		System.out.println(customer1.getId());
 		System.out.println(customer1.getPhoneNumber());
-		System.out.println(customer1.getPaymentInfo());
+		System.out.println(Arrays.toString(customer1.getPaymentInfo()));
 	}
 }
