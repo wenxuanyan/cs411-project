56fb307a43d78a6d2c947d561906985e578e3c6f
Li-Haowei <91715036+Li-Haowei@users.noreply.github.com>
2021-11-02 14:53:44 -0400

    Update rentalSystem.java
    added a method removeRentalBook to remove a current renting record from the rental book.

--- rentalSystem.java
+++ rentalSystem.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 
 public class rentalSystem {
 	private customer[] customerList;
 	private car[] carList;
-	private Object[] rentalBook;
+	private String[][] rentalBook;
 	
 	/**
 	 * Constructor, takes no input and creats a carList abd a customerList
@@ -10,17 +11,17 @@ public class rentalSystem {
 	rentalSystem(){
 		customerList = new customer[10];
 		carList = new car[10];
-		rentalBook = new Object[10];
+		rentalBook = new String[10][4];
 	}
 	/**
-	 * 
+	 * get the customer list
 	 * @return customerList
 	 */
 	public customer[] getCustomerList() {
 		return customerList;
 	}
 	/**
-	 * 
+	 * get the car list
 	 * @return carList
 	 */
 	public car[] getCarList() {
@@ -29,8 +30,12 @@ public class rentalSystem {
 	/**
 	 * add customer into current system
 	 * @param customer
+	 * @throws input error
 	 */
-	public void addCustomer(customer customer) {
+	public void addCustomer(customer customer) throws IllegalArgumentException {
+		if(customer==null) {
+			throw new IllegalArgumentException("addCustomer customer cannot be null");
+		}
 		if(!isFull(customerList)) {
 			customer[] tempList = new customer[customerList.length+10];
 			for(int i=0; i<customerList.length;i++) {
@@ -47,8 +52,12 @@ public class rentalSystem {
 	/**
 	 * add a new car into car list
 	 * @param car
+	 * @throws input error
 	 */
-	public void addCar(car car) {
+	public void addCar(car car) throws IllegalArgumentException {
+		if(car==null) {
+			throw new IllegalArgumentException("addCar car cannot be null");
+		}
 		if(!isFull(carList)) {
 			car[] tempList = new car[carList.length+10];
 			for(int i=0; i<carList.length;i++) {
@@ -65,9 +74,13 @@ public class rentalSystem {
 	/**
 	 * check if the list is full
 	 * @param any(a car list)
+	 * @throws input error
 	 * @return boolean
 	 */
-	public boolean isFull(car[] any) {
+	public boolean isFull(car[] any) throws IllegalArgumentException {
+		if(any==null||any.length==0) {
+			throw new IllegalArgumentException("is Full car list cannot be null or have a length of 0");
+		}
 		for(car s:any) {
 			if(s==null) {
 				return false;
@@ -78,9 +91,13 @@ public class rentalSystem {
 	/**
 	 * check if the list is full
 	 * @param any(a customer list)
+	 * @throws input error
 	 * @return boolean
 	 */
-	public boolean isFull(customer[] any) {
+	public boolean isFull(customer[] any) throws IllegalArgumentException {
+		if(any==null||any.length==0) {
+			throw new IllegalArgumentException("is Full customer list cannot be null or have a length of 0");
+		}
 		for(customer s:any) {
 			if(s==null) {
 				return false;
@@ -91,9 +108,13 @@ public class rentalSystem {
 	/**
 	 * check if the content is in the list(any lists)
 	 * @param content
+	 * @throws input error
 	 * @return boolean
 	 */
-	public boolean isInTheList(String content) {
+	public boolean isInTheList(String content) throws IllegalArgumentException {
+		if(content == null || content.length()==0) {
+			throw new IllegalArgumentException("isInTheList input cannot be null or have a length of 0");
+		}
 		for(customer c:customerList) {
 			if(c!=null) {
 				if(c.getName()==content) return true;
@@ -113,9 +134,13 @@ public class rentalSystem {
 	/**
 	 * delete the car from car list by name or id
 	 * @param content
+	 * @throws input error
 	 * @return boolean
 	 */
-	public boolean delCar(String content) {
+	public boolean delCar(String content) throws IllegalArgumentException{
+		if(content == null || content.length()==0) {
+			throw new IllegalArgumentException("delCar input cannot be null or have a length of 0");
+		}
 		for(car c:carList) {
 			if(c!=null) {
 				if(c.getCarId()==content) {
@@ -133,9 +158,13 @@ public class rentalSystem {
 	/**
 	 * delete the customer from customer list
 	 * @param content
+	 * @throws input error
 	 * @return boolean
 	 */
-	public boolean delCustomer(String content) {
+	public boolean delCustomer(String content) throws IllegalArgumentException{
+		if(content == null || content.length()==0) {
+			throw new IllegalArgumentException("delCar input cannot be null or have a length of 0");
+		}
 		for(customer c:customerList) {
 			if(c!=null) {
 				if(c.getName()==content) {
@@ -158,9 +187,13 @@ public class rentalSystem {
 	 * Input customer ID and the car name or the car ID to rent the car
 	 * If the car is rented, it will output rented
 	 * @param customerID
+	 * @throws input error
 	 * @param content
 	 */
-	public void rentACar(String customerID, String content) {
+	public void rentACar(String customerID, String content) throws IllegalArgumentException{
+		if(customerID==null||content==null||customerID.length()==0||content.length()==0) {
+			throw new IllegalArgumentException("rentACar input cannot be null or have a length of 0");
+		}
 		for(car c: carList) {
 			if(c==null) {
 				System.out.println(content + " is not in the car list");
@@ -178,5 +211,55 @@ public class rentalSystem {
 		}
 		
 	}
-	
+	public void printRentalBook() {
+		System.out.println(Arrays.toString(rentalBook));
+	}
+	/**
+	 * add information of rental into record to keep track of all rentals
+	 * @param customerName
+	 * @param customerId
+	 * @param carName
+	 * @param carId
+	 */
+	public void addRentalBook(String customerName, String customerId, String carName, String carId) throws IllegalArgumentException{
+		if(customerName==null||customerId==null||carName==null||carId==null||customerName.length()==0||customerId.length()==0||carName.length()==0||carId.length()==0) {
+			throw new IllegalArgumentException("addRentalBook input cannot be null or with a length of 0");
+		}
+		for(int i=0;i<rentalBook.length;i++) {
+			if(rentalBook[i][0] == null || rentalBook[i][0].length()==0) {
+				rentalBook[i][1] = customerName;
+				rentalBook[i][2] = customerId;
+				rentalBook[i][3] = carName;
+				rentalBook[i][4] = carId;
+				break;
+			}
+			if(i==rentalBook.length-1) {
+				//In case if rental book is full, extend its capacity
+				String[][] tempList = new String[rentalBook.length+10][4];
+				for(int k=0; k<rentalBook.length;k++) {
+					tempList[k] = rentalBook[k];
+				}
+				rentalBook = tempList;
+			}
+		}
+	}
+	/**
+	 * to remove a rental record from the rental book only needs customer ID and car ID as input
+	 * @param customerId
+	 * @param carId
+	 * @return
+	 */
+	public boolean removeRentalBook(String customerId, String carId) {
+		if(customerId==null||carId==null||customerId.length()==0||carId.length()==0) {
+			throw new IllegalArgumentException("removeRentalBook input cannot be null or with a length of 0");
+		}
+		for(int i=0;i<rentalBook.length;i++) {
+			if(rentalBook[i][1] == customerId || rentalBook[i][3] == carId) {
+				rentalBook[i] = new String[4];
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
